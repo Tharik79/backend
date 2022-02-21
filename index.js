a49658038790ff99b67f8703ff70c2c163f70da1
tharikuthuman <thaarik@gmail.com>
2022-02-21 13:08:08 +0530

    package json edited

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ dotenv.config();
 const app = express();
 //const route = express.Router();
 
-//const port = process.env.PORT || 9000; 
-const PORT = 9000;
-// const MONGO_URL = 'mongodb+srv://Tharik:welcome123@cluster0.ofjdk.mongodb.net/userTable?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 9000; 
+//const PORT = 9000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express());
